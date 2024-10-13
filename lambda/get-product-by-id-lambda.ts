5512b7c56189ff887b95c41dcdb1931133a5f250
Dmytro Livshun <dmytro_livshun@epam.com>
2024-10-13 13:40:53 +0300

    added dynamodb table, post request and fixes

--- lambda/get-product-by-id-lambda.ts
+++ lambda/get-product-by-id-lambda.ts
@@ -1,35 +1,49 @@
-import {wait} from "../shared/helper-functions";
-import {productsMock} from "../lib/product-service";
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+
+const dynamoDB = new DynamoDBClient({ region: process.env.AWS_REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(dynamoDB);
+const tableName = process.env.TABLE_NAME as string;
 
 export async function handler(event: APIGatewayProxyEvent) {
   try {
-    await wait(500)
-
     const id = event.pathParameters?.id
+
+    console.log('event:', event)
 
     if (!id) return {
       statusCode: 400,
       body: JSON.stringify({ message: 'id was not provided' }),
     };
 
-    const product = productsMock.find(product => product.id === id);
+    const command = new GetCommand({
+      TableName: tableName,
+      Key: {
+        id: id,
+      },
+    });
 
-    if (!product) {
+    const result = await ddbDocClient.send(command);
+
+    if (result.Item) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+
+    } else {
       return {
         statusCode: 404,
         body: JSON.stringify({ message: `Product with id:${id} was not found` }),
       }
     }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(product),
-    };
   } catch (error) {
+    console.log('error:', error)
+
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Failed to retrieve products', error }),
     };
   }
-};
+}
